033eec519cc209ce5614e1f4a41ab70daf382c46
Justin Yoo <justin.yoojh@gmail.com>
2025-01-07 19:35:00 +0900

    Add GitHub Model settings

--- src/SKStreaming.ApiApp/Services/KernelService.cs
+++ src/SKStreaming.ApiApp/Services/KernelService.cs
@@ -1,6 +1,4 @@
 using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 
 namespace SKStreaming.ApiApp.Services;
 
@@ -13,20 +11,31 @@ public interface IKernelService
     IAsyncEnumerable<string> CompleteFunctionCallingStreamingAsync(string prompt);
 }
 
-public class KernelService(Kernel kernel) : IKernelService
+public class KernelService(Kernel kernel, IConfiguration config) : IKernelService
 {
     private readonly Kernel _kernel = kernel ?? throw new ArgumentNullException(nameof(kernel));
+    private readonly IConfiguration _config = config ?? throw new ArgumentNullException(nameof(config));
 
     public async Task<string> CompleteChatAsync(string prompt)
     {
-        var result = await this._kernel.InvokePromptAsync(prompt).ConfigureAwait(false);
+#pragma warning disable SKEXP0001
+        var settings = new PromptExecutionSettings { ServiceId = this._config["SemanticKernel:ServiceId"] };
+#pragma warning restore SKEXP0001
+        var arguments = new KernelArguments(settings);
+
+        var result = await this._kernel.InvokePromptAsync(prompt, arguments).ConfigureAwait(false);
 
         return result!.GetValue<string>()!;
     }
 
     public async IAsyncEnumerable<string> CompleteChatStreamingAsync(string prompt)
     {
-        var result = this._kernel.InvokePromptStreamingAsync(prompt).ConfigureAwait(false);
+#pragma warning disable SKEXP0001
+        var settings = new PromptExecutionSettings { ServiceId = this._config["SemanticKernel:ServiceId"] };
+#pragma warning restore SKEXP0001
+        var arguments = new KernelArguments(settings);
+
+        var result = this._kernel.InvokePromptStreamingAsync(prompt, arguments).ConfigureAwait(false);
 
         await foreach (var text in result)
         {
@@ -36,10 +45,16 @@ public class KernelService(Kernel kernel) : IKernelService
 
     public async IAsyncEnumerable<string> CompleteFunctionCallingStreamingAsync(string prompt)
     {
-        var result = this._kernel.InvokePromptStreamingAsync(
-            prompt,
-            new KernelArguments(new OpenAIPromptExecutionSettings { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() })
-            ).ConfigureAwait(false);
+#pragma warning disable SKEXP0001
+        var settings = new PromptExecutionSettings
+        {
+            ServiceId = this._config["SemanticKernel:ServiceId"],
+            FunctionChoiceBehavior = FunctionChoiceBehavior.Auto()
+        };
+#pragma warning restore SKEXP0001
+        var arguments = new KernelArguments(settings);
+
+        var result = this._kernel.InvokePromptStreamingAsync(prompt, arguments).ConfigureAwait(false);
 
         await foreach (var text in result)
         {
